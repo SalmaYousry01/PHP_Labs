423f73fb7607d8b91092675742c6b0fcbb6cb0a9
SalmaYousry01 <syamh26701@gmail.com>
2024-02-27 02:18:32 +0200

    composer added

--- lab2/autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("config.php");
-spl_autoload_register(function($className) {
-    require_once 'Model/' . $className . '.php';
-});
---
+++ lab2/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ea559fb222570b1b45b8965e1ea301c798a19d13',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ea559fb222570b1b45b8965e1ea301c798a19d13',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
